38ea3d5125aa60bc14eeac8f88e80fde43814353
Omni <help.ominc@gmail.com>
2024-08-05 10:16:24 +0500

    feat: implement templates, styles; implement methods for animation, saving and downloading

--- js/main.js
+++ js/main.js
@@ -1,24 +1,70 @@
-import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('[contenteditable]').forEach((element, index) => {
+      saveContent(element, index);
+      materialWave(element);
+  });
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  document.getElementById('download-button').addEventListener('click', () => {
+      getPdf();
+  });
+});
 
-setupCounter(document.querySelector('#counter'))
+function saveContent(element, index) {
+  const savedContent = sessionStorage.getItem('content' + index);
+
+  if (savedContent) {
+      element.innerHTML = savedContent;
+  }
+  element.addEventListener('input', () => {
+      sessionStorage.setItem('content' + index, element.innerHTML);
+  });
+}
+
+function materialWave(element) {
+  element.addEventListener('click', function (event) {
+      const ripple = document.createElement('span');
+      const maxSize = Math.max(this.clientWidth, this.clientHeight);
+      const rect = this.getBoundingClientRect();
+
+      ripple.classList.add('ripple');
+      this.appendChild(ripple);
+      ripple.style.width = ripple.style.height = `${maxSize}px`;
+      ripple.style.left = `${event.clientX - rect.left - maxSize / 2}px`;
+      ripple.style.top = `${event.clientY - rect.top - maxSize / 2}px`;
+
+      ripple.addEventListener('animationend', () => {
+          ripple.remove();
+      });
+  });
+}
+
+function getPdf() {
+  const downloadBtn = document.querySelector('.download-button');
+  const container = document.querySelector('.container');
+  const { jsPDF } = window.jspdf;
+  const doc = new jsPDF('p', 'pt', 'a4');
+  downloadBtn.style.opacity = 0;
+
+  html2canvas(container, {
+      scale: 2
+  }).then(canvas => {
+      const imgData = canvas.toDataURL('image/jpeg', 1.0);
+      const imgWidth = 595.28;
+      const pageHeight = 841.89;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      let heightLeft = imgHeight;
+      let position = 0;
+
+      doc.addImage(imgData, 'JPEG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 0) {
+          position = heightLeft - imgHeight;
+          doc.addPage();
+          doc.addImage(imgData, 'JPEG', 0, position, imgWidth, imgHeight);
+          heightLeft -= pageHeight;
+      }
+      doc.save('resume.pdf');
+      downloadBtn.style.opacity = 1;
+  });
+}
